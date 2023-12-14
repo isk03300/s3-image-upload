a813d790905b96a7cf7a5d097f949988066ed1ae
isk03300 <isk03300@naver.com>
2023-12-14 13:09:04 +0900

    add

---
+++ mysql_connection.py
@@ -0,0 +1,11 @@
+import mysql.connector
+from config import Config
+
+def get_connection():
+    connection = mysql.connector.connect(
+        host = Config.HOST,
+        database = Config.DATABASE,
+        user = Config.DB_USER,
+        password = Config.PASSWORD
+    )
+    return connection
